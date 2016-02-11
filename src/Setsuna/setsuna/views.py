221b46867f3a982edc1f7085d5f50b0bf75fbb9d
manzyun <manzyun@gmail.com>
2016-02-11 19:24:12 +0900

    ドキュメント加筆

--- src/Setsuna/setsuna/views.py
+++ src/Setsuna/setsuna/views.py
@@ -4,3 +4,7 @@ from pyramid.view import view_config
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
     return {'project': 'Setsuna'}
+
+@view_config(route_name='hello')
+def hello(request):
+    return Response('<body>Go back <a href="/">home</a></body>')
