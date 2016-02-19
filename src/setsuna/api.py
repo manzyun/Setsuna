52eb516c1b689040b9cab6c5892ec35692b39afd
Takahashi Hidetsugu <manzyun@gmail.com>
2016-02-19 17:37:42 +0900

    API手直し"

--- src/setsuna/api.py
+++ src/setsuna/api.py
@@ -1,6 +1,6 @@
 import json
 from setsuna import app
-from flask_restful import response, abort, Api, Resource
+from flask_restful import reqparse, abort, Api, Resource
 
 api = Api(app)
 
@@ -10,18 +10,19 @@ def abort_if_post_doesnot_exist(unique_id):
         abort(404, message="Post {} is nothing. Maybe, This post was delete \
                             or die.")
 
-parser = reqperse.RequestParser()
-parser.add_argment("content")
-parser.add_argment("delkey")
-parser.add_argment("unique_id")
+parser = reqparse.RequestParser()
+parser.add_argument("content")
+parser.add_argument("delkey")
+parser.add_argument("unique_id")
 
-class Post(Resource):
-    def get(self, unique_id):
-        
-        abort_if_post_doesnot_exist(unique_id)
-        post = app.DATABASE.find({"unique_id:": unique_id})
-        del post["_id"]
-        return json.dumps(post)
+class Posts(Resource):
+    def get(self):
+        args = parser.parse_args()
+        posts = app.DATABASE.find().limit(args['limit'])
+        for post in posts:
+            del post["_id"]
+
+        return posts
 
     def post(self):
         args = parser.parse_args()
@@ -29,10 +30,16 @@ class Post(Resource):
         post.content = args["content"]
         post.delkey = args["delkey"]
         post.post()
-
         return post_content, 201
 
-    def delete(self):
+class Post(Resource):
+    def get(self, unique_id):
+        abort_if_post_doesnot_exist(unique_id)
+        post = app.DATABASE.find({"unique_id:": unique_id})
+        del post["_id"]
+        return json.dumps(post)
+
+    def delete(self, unique_id):
         args = parser.parse_args()
         abort_if_post_doesnot_exist(args["unique_id"])
         post = app.Content(args["unique_id"])
@@ -41,3 +48,6 @@ class Post(Resource):
         else:
             return "Unmatched password.", 200
 
+# Actualy setup the Api resource routing
+api.add_resource(Posts, '/api/1/')
+api.add_resource(Post, '/api/1/<unique_id>')
