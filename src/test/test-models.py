4d5fe584057f1f680816a2e8308ff6357583955b
Takahashi Hidetsugu <manzyun@gmail.com>
2016-02-25 12:30:50 +0900

    やっとまともにテストが動くようになった……ハズ

--- src/test/test-models.py
+++ src/test/test-models.py
@@ -5,27 +5,24 @@ import json
 import datetime
 import calendar
 
+testdata = {"unique_id": 1,
+            "content": "美味しい美味しいスープカレー",
+            "limit": calendar.timegm(
+                datetime.datetime.utcnow().timetuple()),
+            "delkey": "hogefuga"
+            }
+
 class TestPost(TestCase):
+    client = MongoClient(conf._conf["address"], conf._conf["port"])
+    db = client[conf._conf["database"]]
+    collection = db[conf._conf["collection"]]
 
-    @classmethod
-    def setup_class(self):
-        client = MongoClient(conf._conf["address"], conf._conf["ip"])
-        db = client[conf._conf["db"]]
-        collection = db[conf._conf["collection"]]
-
-        testdata = {"unique_id":
-                     collection.find_one({}, {unique_id: 1, _id: 0}) + 1,
-                    "content": "美味しい美味しいスープカレー",
-                    "limit": calendar(
-                        timegm(datetime.datetime.utcnow().timetuple())),
-                    "delkey": "hogefuga"
-                   }
     def setup(self):
         # とりあえず書く
-        self.testindex = collection.insert_one(testdata)
+        self.testindex = self.collection.insert_one(testdata)
 
     def teardown(self):
-        collection.remove({"_id": self.testindex})
+        self.collection.remove({"_id": self.testindex})
 
     def test_make_model(self):
         """ モデルが読み込まれてインスタンスが生成されるか """
@@ -44,19 +41,19 @@ class TestPost(TestCase):
         self.assertEqual(model, model_sample)
 
 
-    @expectedFailure
     def test_delete_model(self):
         """ 狙ったレコードがパスワードが合致した場合に削除されるか """
         model = models.Post(testdata["unique_id"])
         model.delete(testdata["delkey"])
 
-        collection.find_one({"unique_id": testdata["unique_id"]})
+        self.assertRaises(NoneRecordException,
+                self.collection.find_one({"unique_id": testdata["unique_id"]}))
 
-    @expectedFailure
     def test_dead_model(self):
         """ リミットオーバーした場合投稿が削除されるか """
         model = models.Post(testdata["unique_id"])
         model.apoptosis()
 
-        collection.find_one({"unique_id": testdata["unique_id"]})
+        self.assertRaises(NoneRecordException,
+            self.collection.find_one({"unique_id": testdata["unique_id"]}))
 
