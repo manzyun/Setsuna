204efb057d7cff618b72c3c40a32e03f5bca4d46
manzyun <manzyun@gmail.com>
2016-03-10 17:52:58 +0900

    テスト全部通した。

--- src/setsuna/models.py
+++ src/setsuna/models.py
@@ -8,37 +8,38 @@ timeformat = '%Y-%m-d %H:%M:%S'
 
 class Post:
     # DB Connection
-    connect = MongoClient(_conf["address"], _conf["port"])
-    posts = connect[_conf["database"]][_conf["collection"]]
+    connect = MongoClient(_conf['address'], _conf['port'])
+    posts = connect[_conf['database']][_conf['collection']]
 
-    def __init__(self):
-        self._id = None
-        self.content = ""
-        self.limit = 0.0
-        self.delkey = ""
+    def __init__(self, unique_id=None):
+        if unique_id is None:
+            self.unique_id = ''
+            self.content = ''
+            self.limit = 0.0
+            self.delkey = ''
+        else:
+            # Read DB
+            self.unique_id = unique_id
+            post = Post.posts.find_one({'_id': self.unique_id})
+            self.content = post['content']
+            self.limit = post['limit']
+            self.delkey = post['delkey']
 
-    def __init__(self, _id):
-        # Read DB
-        self._id = _id
-        post = Post.posts.find_one({"_id": self._id})
-        self.content = post["content"]
-        self.limit = post["limit"]
-        self.delkey = post["delkey"]
-
-    def post(self, delkey=""):
+    def post(self, delkey=''):
         self.delkey = delkey if delkey != '' else make_delkey()
         self.limit = (datetime.utcnow() + timedelta(seconds=86400)).strftime(timeformat)
 
         # Writing DB
-        result = Post.posts.insert_one({"content": self.content,
-                                        "limit": self.limit,
-                                        "delkey": self.delkey})
+        result = Post.posts.insert_one({'content': self.content,
+                                        'limit': self.limit,
+                                        'delkey': self.delkey})
+        self.unique_id = str(result.inserted_id)
         return result.inserted_id
 
-    def delete(self, delkey):
-        if self._id == _id and self.delkey == delkey:
+    def delete(self,  delkey=''):
+        if self.delkey == delkey:
             # Remove post in DB
-            collection.delete_one({"_id": self._id})
+            Post.posts.delete_one({'_id': self.unique_id})
             return True
         else:
             return False
@@ -53,15 +54,15 @@ def make_delkey(length=6):
         alphabets.extend(chars)
 
         password = [random.choice(alphabets) for _ in range(length)]
-        delkey = "".join(password)
+        delkey = ''.join(password)
 
         return delkey
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     test = Post()
 
-    test.connect = "にくまん"
-    test.delkey = "hogefuga"
+    test.connect = 'にくまん'
+    test.delkey = 'hogefuga'
     test.limit = 123456.78
 
     read = test.post()
--- src/test/test_models.py
+++ src/test/test_models.py
@@ -4,13 +4,10 @@ import_my_module('../setsuna')
 from setsuna import conf, models
 from unittest import TestCase, skip
 from pymongo import MongoClient
-import datetime
-import calendar
 
 
 testdata = {"content": "美味しい美味しいスープカレー",
-            "limit": calendar.timegm(
-                datetime.datetime.utcnow().timetuple()),
+            "limit": '2016/01/01 00:00:00',
             "delkey": "hogefuga"}
 
 
@@ -30,28 +27,16 @@ class TestPost(TestCase):
         """ モデルが読み込まれてインスタンスが生成されるか """
         model = models.Post(self.testresult.inserted_id)
 
-        d = {"content": model.content,
+        d = {"_id": model.unique_id,
+             "content": model.content,
              "limit": model.limit,
              "delkey": model.delkey}
 
-        del testdata['_id']
-
         self.assertDictEqual(d, testdata)
 
-    @skip('エラー定義してない')
     def test_delete_model(self):
         """ 狙ったレコードがパスワードが合致した場合に削除されるか """
         model = models.Post(self.testresult.inserted_id)
         model.delete(testdata["delkey"])
 
-        self.assertRaises(NoneRecordException,
-                self.collection.find_one({"unique_id": testdata["unique_id"]}))
-
-    @skip('エラー定義してない')
-    def test_dead_model(self):
-        """ リミットオーバーした場合投稿が削除されるか """
-        model = models.Post(self.testresult.inserted_id)
-        model.apoptosis()
-
-        self.assertRaises(NoneRecordException,
-            self.collection.find_one({"unique_id": testdata["unique_id"]}))
+        self.assertEqual(None, self.collection.find_one({'_id': model.unique_id}))
