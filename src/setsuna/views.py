e37afe350afb0bdcbb3f7d066b3ff2d28e68c282
Takahashi Hidetsugu <manzyun@gmail.com>
2016-04-07 16:43:47 +0900

    粗くテンプレートを書いた。

--- src/setsuna/views.py
+++ src/setsuna/views.py
@@ -4,9 +4,17 @@ from flask import render_template
 
 @app.route('/')
 def index():
+    news = []
+    bson_news = conf.posts.find().sort({'timestamp': 1})
     return render_template('views/index.html')
 
 
 @app.route('/<unique_id>')
-def post(unique_id):
+def content(unique_id):
+
     return render_template('views/unique.html')
+
+
+@app.route('/new')
+def post():
+    return render_template('views/new.html')
