ca1973497715fdeb711bb15fdacb9c78d639e23d
manzyun <manzyun@gmail.com>
2016-07-27 16:12:19 +0900

    Make db module

--- src/setsuna/api.py
+++ src/setsuna/api.py
@@ -1,6 +1,5 @@
-import json
 from datetime import datetime
-from . import app, models, conf
+from . import app, models
 from pymongo import MongoClient
 from flask import abort, jsonify, Request, request, Response
 
@@ -36,118 +35,7 @@ def know_post(unique_id):
 
 @app.route('/', methods=['GET'])
 def index():
-    return '''
-====================
-Setsuna Web API
-====================
-
-author
--------------
-
-Hidetsugu Takahashi a.k.a manzyun <manzyun@gmail.com>
-
-
-What's this?
----------------------
-
-This is Micro SNS. Your post will delete 6 hour.
-
-But, your post is gotten response of 1 hour plus every,
-
-
-How to use
---------------
-
-Contribution
-~~~~~~~~~~~~~~
-
-1. Make JSON::
-
-  {
-    "content":your content here,
-    "password": you want delete contribution password.
-    "lang": Language code by ISO 639-2.
-  }
-
-.. note:: If "password" is nothing then make random 4 digit password.
-
-2. Contribute this address your JSON on POST method.
-3. 'Comming server response your contribut infomartion in "data" section::
-
-  {
-    "id": Your contribution id,
-    "content": You contribute content,
-    "password": Delete contribution password,
-    "lang": Language code by ISO 639-2.
-  }
-
-
-Response(no_comment)
-~~~~~~~~~~~~~~~~~~~~~~~
-
-1. Access contribution on POST method.
-2. Comming server response your responsed contribution information in "data" section::
-
-  {
-    "id": Your responsed contribution id,
-    "content": Your responsed contribution
-  }
-
-
-Response(comment)
-~~~~~~~~~~~~~~~~~~~~
-
-1. Make JSON::
-
-  {
-    "content":your content here,
-    "password": you want delete contribution password,
-    "lang": Language code by ISO 639-2.
-  }
-
-.. note:: If "password" is nothing then make random 4 digit password.
-
-
-2. Contribute you want response contribution address on POST method
-3. Comming server response your contribution information in "data" section::
-
-  {
-    "id": Your contribution id,
-    "content": You contribute content,
-    "password": Delete contribution password,
-    "lang": Language code by ISO 639-2.
-  }
-
-
-Delete Contribution
-~~~~~~~~~~~~~~~~~~~~~~
-
-1. Make JSON::
-
-  {
-    "password": your contribution has delete password.
-  }
-
-
-2. Contribute this address your JSON on DELETE method.
-3. 'Comming server response your contribute infomartion in "data" section::
-
-  {
-    "message": "Your post deleted ;)"
-  }
-
-
-.. note:: If contribution has response then delete with responsed contribution.
-
-FAQ
------
-
-Where web page?
-  Nothing special. Because, I want make a service and I am poor sense web design. Because so only Web and JSON API',
-
-Am I not user requiring?
-  Yes. This service is anonymity. And I can not manage a server that is managing the large amount of user information.'
-'''
+    return 
 
 
 def make_template(json_id=1) -> str:
@@ -256,7 +144,7 @@ def tell_post(unique_id):
         return json.dumps(res)
 
 
-@app.route('/post/<unique_id>', methods=['DELETE'])
+@app.route('/<unique_id>', methods=['DELETE'])
 def delete_post(unique_id):
     if know_post(unique_id):
         if not Request.get_json(request):
@@ -271,7 +159,7 @@ def delete_post(unique_id):
                 return json.dumps({'result': False, 'message': 'Not matching password.'})
 
 
-@app.route('/post', methods=['POST'])
+@app.route('/', methods=['POST'])
 def post_content():
     if not Request.get_json(request):
         abort(400)
@@ -280,26 +168,10 @@ def post_content():
     if not 'content' in req:
         abort(400)
 
-    post = models.Post()
-    post.content = req['content']
-    post.password = req['password'] if 'password' in req else make_password()
-    result = post.post()
-
     # Create response data.
     res = conf.posts.find_one({'_id': result})
     res['_id'] = str(res['_id'])
     return json.dumps(res)
 
 
-def make_password(length=6):
-    # Make font map
-    alphabets = []
-    codes = (('a', 'z'), ('A', 'Z'), ('0', '9'))
-    for r in codes:
-        chars = map(chr, range(ord(r[0]), ord(r[1]) + 1))
-        alphabets.extend(chars)
 
-        password = [random.choice(alphabets) for _ in range(length)]
-        password = ''.join(password)
-
-        return password
--- src/setsuna/models.py
+++ src/setsuna/models.py
@@ -1,9 +1,12 @@
 import typing
-
+from bson import objectid
+from time import time
+from . import conf, db
 
 '''
 This module is model defining for Setsuna.  
 '''
+
 
 class Post:
 '''
@@ -15,7 +18,7 @@ limit -- Delete time. Record style is Unix time.
 password -- Password for manually delete.
 lang -- Language code by ISO 639-2.  
 '''
-    def __init__(self, content: str, limit: int, password: str, lang: str) -> None:
+    def __init__(self, content: str, password: str, lang: str):
     '''
     Make post.  
 
@@ -25,25 +28,84 @@ lang -- Language code by ISO 639-2.
     lang -- Language code by ISO 639-2.  
     '''
         self.content = content
-        self.limit = limit
-        self.password = password
+        self.limit = time()
+        self.password = password if '' self.make_password() else password
         self.lang = lang
+
+
     def __str__(self) -> str:
         return str(self.__dict__)
+
+
     def __repr__(self) -> str:
         return str(self.__dict__)
+
+
     def __iter__(self):
         return self.__dict__.iteritems()
-    def __getitem__(self, key: str) -> int ~ str:
+
+
+    def __getitem__(self, key: str):
         return self.__dict__[key]
-    def __setitem__(self, key: str, value) -> None:
-        self.__dict__[key] = value
-    def add_id(self, uid: int) -> None:
+
+
+    def read_from_db(self, uid: str):
     '''
     Append identity key.
+
     uid -- identity key from DB
     '''
         self.id = uid
+
+
+    def rewrite_limit(self, limit:float):
+    '''
+    Rewrite limit
+    
+    limit -- new limit time
+    '''
+        self.limit = limit
+
+
+    def post_contribution() -> str:
+    '''
+    Post contribution to DB.  
+
+    return -- identity key from DB
+    '''
+        result = db.write_post(self)
+        self.add_id(result)
+        return self.add_id
+
+
+    def apothanasia():
+    '''
+    Contribution prolonging life.  
+
+    return -- new delete limit time.
+    '''
+        self.limit = self.limit + 3600
+        db.apothanasia(self)
+
+
+    def make_password(length=4: int) -> str:
+    '''
+    Make password
+
+    length -- Make password digit.  
+    return -- Password.  
+    '''
+        # Make font map
+        alphabets = []
+        codes = (('a', 'z'), ('A', 'Z'), ('0', '9'))
+        for r in codes:
+            chars = map(chr, range(ord(r[0]), ord(r[1]) + 1))
+            alphabets.extend(chars)
+
+            password = [random.choice(alphabets) for _ in range(length)]
+            password = ''.join(password)
+
+            return password
 
 
 class ResponsePost(Post):
@@ -57,7 +119,7 @@ limit -- Delete time. Record style is Unix time.
 password -- Password for manually delete.
 lang -- Language code by ISO 639-2.  
 '''
-    def __init__(self, link: int, content: str, limit: int, password: str, lang: str) -> None:
+    def __init__(self, link: str, content: str, password: str, lang: str):
     '''
     Make response post.  
 
@@ -69,14 +131,20 @@ lang -- Language code by ISO 639-2.
     lang -- Language code by ISO 639-2.  
     '''
         self.link = link 
-        Post.__init__(content, limit, password, lang)
-    def __str__(self) -> str:
-        return str(self.__dict__)
-    def __repr__(self) -> str:
-        return str(self.__dict__)
-    def __iter__(self):
-        return self.__dict__.iteritems()
-    def __getitem__(self, key: str) -> int ~ str:
-        return self.__dict__[key]
-    def __setitem__(self, key: str, value) -> None:
-        self.__dict__[key] = value
+        Post.__init__(content, password, lang)
+
+
+    def post_response() -> str:
+    '''
+    Post contribution to DB.  
+
+    return -- identity key from DB
+    '''
+        result = db.write_response(self)
+
+        # apothanasia linked contribution.
+        linked_post = db.read_from_db(self.link)
+        linked_post.apothanasia()
+
+        self.add_id(str(result.inserted_id))
+        return self.add_id
